7c55c1bc8f8e8d8cc1051aec7a9825c08cc8a686
Edwaldo Utama <53479749+edwaldo12@users.noreply.github.com>
2025-01-11 21:39:53 +0700

    f9xo9

--- src/dto/auth.dto.ts
+++ src/dto/auth.dto.ts
@@ -2,34 +2,34 @@ import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 import { z } from 'zod';
 
 export class RegisterDTO {
-    @IsNotEmpty({ message: 'name is not empty' })
-    name: string;
+  @IsNotEmpty({ message: 'name is not empty' })
+  name: string;
 
-    @IsNotEmpty()
-    @IsEmail()
-    email: string;
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
 
-    @IsNotEmpty()
-    @MinLength(8)
-    password: string;
+  @IsNotEmpty()
+  @MinLength(8)
+  password: string;
 }
 export class LoginDTO {
-    @IsNotEmpty()
-    @IsEmail()
-    email: string;
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
 
-    @IsNotEmpty()
-    @MinLength(8)
-    password: string;
+  @IsNotEmpty()
+  @MinLength(8)
+  password: string;
 }
 
 export const authRegisterRequestSchema = z.object({
-    name: z.string().min(1, 'name is required'),
-    email: z.string().min(1, 'email is required'),
-    password: z.string().min(1, 'password is required'),
+  name: z.string().min(1, 'name is required'),
+  email: z.string().min(1, 'email is required'),
+  password: z.string().min(1, 'password is required'),
 });
 
 export const authLoginRequestSchema = z.object({
-    email: z.string().min(1, 'email is required'),
-    password: z.string().min(1, 'password is required'),
+  email: z.string().min(1, 'email is required'),
+  password: z.string().min(1, 'password is required'),
 });
